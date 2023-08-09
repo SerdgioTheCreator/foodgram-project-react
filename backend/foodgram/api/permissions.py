35229dd85f275998125d72fa4909b446cc9ab662
Сергей Коновалов <SokiZNosoK68rus@yandex.ru>
2023-08-09 13:02:39 +0300

    changed nginx.conf and permissions.py

--- backend/foodgram/api/permissions.py
+++ backend/foodgram/api/permissions.py
@@ -1,16 +1,13 @@
-from rest_framework import permissions
+from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 
-class IsOwnerOrReadOnly(permissions.BasePermission):
+class IsOwnerOrReadOnly(BasePermission):
 
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return obj.owner == request.user
+        return request.method in SAFE_METHODS or obj.author == request.user
 
 
-class IsAdminOrReadOnly(permissions.BasePermission):
+class IsAdminOrReadOnly(BasePermission):
     def has_permission(self, request, view):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return request.user and request.user.is_staff
+        return (request.method in SAFE_METHODS
+                or request.user and request.user.is_staff)
